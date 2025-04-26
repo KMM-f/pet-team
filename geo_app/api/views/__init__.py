35ec8abb3d87933eb0ecbb91a405ad402bb31a25
good4g <gydk102@gmail.com>
2025-04-26 17:13:40 +0300

    rename evet -> event_view.py

--- geo_app/api/views/__init__.py
+++ geo_app/api/views/__init__.py
@@ -1,4 +1,4 @@
 from .room_view import RoomList, CreateRoom, RoomDetail
 from .marker_view import CreateMarker, MarkerDetail
-from .event_view import CreateEvent, EventDetail
 from .user_view import CreateUser, UserDetail
+from .event_view import CreateEvent, EventDetail
